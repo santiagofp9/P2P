bfc25cdb039b2275f02533ed77fbfafbe0485d1c
santiagofp9 <57355122+santiagofp9@users.noreply.github.com>
2019-12-17 13:55:19 +0100

    Delete eliminar_coder.php

--- grupal3/php/eliminar_coder.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$cod=$_GET['eli'];
-
-	$slq_eliminar="UPDATE coder SET estado='0' WHERE coder.id_coder='$cod'";
-
-	include 'conexion.php';
-	mysqli_query($link,$slq_eliminar);
-	header('location:coders.php');
-?>
