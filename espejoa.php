83717c08b365a1f2f68ac586ed2d0f1e38b015e9
santiagofp9 <57355122+santiagofp9@users.noreply.github.com>
2019-12-18 11:13:03 +0100

    Update espejoa.php

--- espejoa.php
+++ espejoa.php
@@ -1,32 +1,26 @@
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-       
-	<div class="navbar-header">
-    	<a class="navbar-brand" href="./">TOMILLO <sup><small><span class="label label-warning">F5</span></small></sup> </a>
-	</div>
-    <div class="collapse navbar-collapse navbar-ex1-collapse">
-    	<ul class="nav navbar-nav side-nav">
-        	<ul class="nav navbar-nav">
-        	</ul>
-        		<li><a href="index.php"><i class="fa fa-home"></i> Inicio</a></li>
-          		<li><a href="php/fabricas.php"><i class='fa fa-tasks'></i> Fabricas </a></li>
-          		<li><a href="php/promociones.php"><i class="fa fa-users"></i> Promociones </a></li>
-          		<li><a href="php/coders.php"><i class="fa fa-smile-o"></i> Coders </a></li>
-		<?php
-     			if(isset($_SESSION['nomb'])){
-         		echo "Bienvenido ".$_SESSION['nomb'];
-     	?>
-         		<section>
-             		<nav>
-                 		<a href="registro.php" class="btn btn-primary" role="button">Registrar</a>
-                 		<form action="controlador.php" method="post" class="formlogin">
-    						<input type="hidden" name="oculto" value="3">
-    						<button type="submit" class="btn btn-success">Cerrar Sesión</button> 
-						</form> 
-             		</nav>
-                 </section>
-		<?php } ?>
-          		</ul>
 
-    
-    </div>
-    </nav>
+    	<ul class="nav navbar-nav side-nav">	
+        	<li><a href="index.php"><i class="fa fa-home"></i> Inicio</a></li>
+          	<li><a href="php/fabricas.php"><i class='fa fa-tasks'></i> Fabricas </a></li>
+          	<li><a href="php/promociones.php"><i class="fa fa-users"></i> Promociones </a></li>
+          	<li><a href="php/coders.php"><i class="fa fa-smile-o"></i> Coders </a></li>			
+        </ul>
+		<ul class="nav navbar-nav navbar-right navbar-user">
+        	<li class="dropdown user-dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+					<?php if(isset($_SESSION['nomb'])){
+         				echo "Bienvenido ".$_SESSION['nomb'];?>
+					<b class="caret"></b>
+				</a>
+             	<ul class="dropdown-menu">
+					<nav>
+						<li><a href="registro.php" class="btn" role="button">Registrar</a></li>
+                 		<form action="controlador.php" method="post" class="formlogin">
+							<li></li><input type="hidden" name="oculto" value="3"></li>
+							<li></li><button type="submit" class="btn">Cerrar Sesión</button></li> 
+						</form> 
+             		</nav> 
+					<?php } ?>
+             	</ul>
+             </li>
+          </ul>
